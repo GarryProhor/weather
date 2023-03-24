e15cd33cde467e228c771288fa87508264d7f462
GarryProhor <igorprohorchenko@gmail.com>
2023-03-24 17:08:39 +0300

    all fetch url configuration

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,4 +5,40 @@
  * @author GarryProhor <igorprohorchenko@gmail.com>
  */
 
+'use strict';
+
 const api_key = "b67f39f982a59f8dd63aeaed11ec4a8a";
+
+/**
+ *
+ * @param {string} URL Api url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback){
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/data/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     *
+     * @param {string} query Search query e.g.: "London", "New York"
+     */
+    geo(query) {
+        return `https://api.openweathermap.org/data/1.0/direct?q=${query}&limit=5`
+    }
+}
