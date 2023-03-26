6dc7f465840ef0c2bff465f0b7290e1be934be23
GarryProhor <igorprohorchenko@gmail.com>
2023-03-26 15:40:21 +0300

    current weather API integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,11 @@
 
 
 import {fetchData, url} from "./api.js";
+import * as module from './module.js';
 
 /**
  * Add event listener on multiple elements
- * @param {NodeList} elements Elements node array
+ * @param {*[]} elements Elements node array
  * @param {string} eventType Event Type e.g.: "click", "mouseover"
  * @param {Function} callback Callback function
  */
@@ -44,10 +45,8 @@ searchField.addEventListener("input", function (){
         searchResult.classList.remove("active");
         searchResult.innerHTML = "";
         searchField.classList.remove("searching");
-        console.log("error")
     }else {
         searchField.classList.add("searching");
-        console.log(searchField.value)
     }
 
     if(searchField.value){
@@ -62,6 +61,7 @@ searchField.addEventListener("input", function (){
 
                 let items = [];
                 for (const {name, lat, lon, country, state} of locations){
+
                     const searchItem = document.createElement("li");
                     searchItem.classList.add("view-item");
                     searchItem.innerHTML = `
@@ -77,7 +77,97 @@ searchField.addEventListener("input", function (){
 
                     items.push(searchItem.querySelector("[data-search-toggler]"));
                 }
+
+                addEventOnElements(items, 'click', function (){
+                   toggleSearch();
+                   searchResult.classList.remove("active");
+                });
             });
         }, searchTimeoutDuration);
     }
 });
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector("[data-current-location-btn]");
+const errorContent = document.querySelector("[data-error-content]");
+
+/**
+ * Render all weather data in html page
+ * @param {number} lat Latitude
+ * @param {number} lon Longitude
+ */
+export const updateWeather = function (lat, lon) {
+
+    // loading.style.display = "grid";
+    container.style.overflowY = "hidden";
+    // container.classList.remove("fade-in");
+    errorContent.style.display = "none";
+
+    const currentWeatherSection = document.querySelector("[data-current-weather]");
+    const highlightSection = document.querySelector("[data-highlights]");
+    const hourlySection = document.querySelector("[data-hourly-forecast]");
+    const forecastSection = document.querySelector("[data-5-day-forecast]");
+
+    currentWeatherSection.innerHTML = "";
+    highlightSection.innerHTML = "";
+    hourlySection.innerHTML = "";
+    forecastSection.innerHTML = "";
+
+    if(window.location.hash === "#/current-location"){
+        currentLocationBtn.setAttribute("disabled", "");
+    }else {
+        currentLocationBtn.removeAttribute("disabled");
+    }
+
+    /**
+     * CURRENT WEATHER SECTION
+     */
+
+    fetchData(url.currentWeather(lat, lon), function (currentWeather){
+        const {
+            weather,
+            dt: dateUnix,
+            sys: {sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC},
+            main: {temp, feels_like, pressure, humidity},
+            visibility,
+            timezone
+        } = currentWeather;
+        const [{description, icon}] = weather;
+        console.log(weather)
+
+        const  card = document.createElement("div");
+        card.classList.add("card", "card-lg", "current-weather-card");
+        card.innerHTML = `
+            <h2 class="title-2 card-title">Now</h2>
+            <div class="weapper">
+                <p class="heading">${parseInt(temp)}&deg;<sup>c</sup></p>
+                <img src="./assets/images/weather_icons/${icon}.png"
+                     alt="${description}" width="64" height="64" class="weather-icon">
+            </div>
+            <p class="body-3">${description}</p>
+            <ul class="meta-list">
+                 <li class="meta-item">
+                      <span class="m-icon">calendar_today</span>
+                      <p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+                 </li>
+
+                 <li class="meta-item">
+                       <span class="m-icon">location_on</span>
+                       <p class="title-3 meta-text" data-location></p>
+                 </li>
+            </ul>
+        `;
+
+        fetchData(url.reverseGeo(lat, lon), function ([{name, country}]) {
+            card.querySelector("[data-location]").innerHTML = `${name}, ${country}`;
+        });
+        currentWeatherSection.appendChild(card);
+    });
+
+}
+
+export const  error404 = function () {
+
+}
+
